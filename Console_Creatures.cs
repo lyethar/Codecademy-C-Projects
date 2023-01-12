300e5dda84778e995f6480379c5a2ab1cbd0f336
lyethar <107083757+lyethar@users.noreply.github.com>
2023-01-12 11:55:34 -0500

    Create Console_Creatures.cs
    First project: Console Creatures. The project revolved around playing around with Console.WriteLine to make ASCII art. Cool for learning more about banners. I also found this website: https://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20

---
+++ Console_Creatures.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      /* This program prints different creasures for funsies */
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");      
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("My name Casper");
+     }
+  }
+}
